e721c4907010202d9cabcea96b06c56b576fe48e
Eugene Kim <ek@harmony.one>
2018-12-18 19:54:20 -0800

    Move NewEncoder/NewDecoder into defaults
    This is to break an import cycle.

--- pkg/defaults/default.go
+++ pkg/defaults/default.go
@@ -12,3 +12,22 @@ func DefaultEncoderFactory() raptorq.EncoderFactory {
 func DefaultDecoderFactory() raptorq.DecoderFactory {
 	return &libraptorq.DecoderFactory{}
 }
+
+// NewEncoder creates and returns an encoder using the default factory.
+func NewEncoder(
+	input []byte, symbolSize uint16, minSubSymbolSize uint16,
+	maxSubBlockSize uint32, alignment uint8,
+) (enc raptorq.Encoder, err error) {
+	factory := DefaultEncoderFactory()
+	return factory.New(
+		input, symbolSize, minSubSymbolSize, maxSubBlockSize, alignment,
+	)
+}
+
+// NewDecoder creates and returns a decoder using the default factory.
+func NewDecoder(commonOTI uint64, schemeSpecificOTI uint32) (
+	dec raptorq.Decoder, err error,
+) {
+	factory := DefaultDecoderFactory()
+	return factory.New(commonOTI, schemeSpecificOTI)
+}
